8d371ff4200b92a4f2e23de980f3e80cbca163b1
zephiii <jte@duck.com>
2025-11-23 20:07:44 +0000

    semantic versioning

--- public/client/app.js
+++ public/client/app.js
@@ -8,7 +8,7 @@ import * as socketStuff from "./socketinit.js";
 
 (async function (util, global, config, Canvas, color, gameDraw, socketStuff) {
     let { socketInit, resync, gui, leaderboard, minimap, moveCompensation, lag, getNow } = socketStuff;
-    let buildNumber = "v2.0.70531";
+    let buildNumber = "v2.0.0-rev.70532";
     // Get the changelog
     fetch("changelog.md", { cache: "no-cache" }).then(response => response.text()).then(response => {
         let a = [];
@@ -253,7 +253,7 @@ import * as socketStuff from "./socketinit.js";
                 return d;
             } else throw new Error("Invalid menu tab type.");
         };
-        global.createTabMenu(`Unstable branch, Build: ${buildNumber}`, "warning");
+        global.createTabMenu(`Unstable branch (build: ${buildNumber})`, "warning");
         // Warn the users to turn their phones into landscape.
         if (global.mobile && window.innerHeight > 1.1 * window.innerWidth) {
             let tabMenu = global.createTabMenu("Please turn your device to landscape mode.", "warning", true);
