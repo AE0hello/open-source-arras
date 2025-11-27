01be87f2c1e248bf83dc89dd1e6522811d2f5233
zenphia <93544652+zephiii@users.noreply.github.com>
2025-11-27 19:26:02 +0000

    why was this here

--- server/lib/definitions/groups/dev.js
+++ server/lib/definitions/groups/dev.js
@@ -1,4 +1,4 @@
-const { combineStats, makeMenu, addAura, makeDeco, LayeredBoss, newWeapon, weaponArray, makeRadialAuto, makeTurret, makeAuto } = require('../facilitators.js')
+const { combineStats, makeMenu, addAura, makeDeco, LayeredBoss, weaponArray, makeRadialAuto, makeTurret, makeAuto } = require('../facilitators.js')
 const { base, basePolygonDamage, basePolygonHealth, dfltskl, statnames } = require('../constants.js')
 const g = require('../gunvals.js')
 require('./tanks.js')
