50979f7593f9741ffa3fbc74db21b919624990e5
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2025-12-04 23:32:28 -0700

    Update walls.js
    wall colors

--- server/lib/definitions/groups/walls.js
+++ server/lib/definitions/groups/walls.js
@@ -14,27 +14,31 @@ Class.wall = {
 Class.deadlyWall = {
     PARENT: "wall",
     LABEL: "Deadly Wall",
-    COLOR: "red"
+    COLOR: 12
 }
 
 Class.healingWall = {
     PARENT: "wall",
-    LABEL: "Healing Wall"
+    LABEL: "Healing Wall",
+    COLOR: 11
 }
 
 Class.bouncyWall = {
     PARENT: "wall",
-    LABEL: "Bouncy Wall"
+    LABEL: "Bouncy Wall",
+    COLOR: 19
 }
 
 Class.breakerWall = {
     PARENT: "wall",
-    LABEL: "Breaker Wall"
+    LABEL: "Breaker Wall",
+    COLOR: 5
 }
 
 Class.chunksWall = {
     PARENT: "wall",
-    LABEL: "Chunks Wall"
+    LABEL: "Chunks Wall",
+    COLOR: 4
 }
 
 Class.opticalWall = {
@@ -45,67 +49,106 @@ Class.opticalWall = {
             POSITION: [14, 0, 0, 0, 360, 1],
             TYPE: "eyeturret"
         }
-    ]
-}
-
-Class.oneWayWallUp = {
-    PARENT: "wall",
-    LABEL: "One-Way Wall (Up)"
+    ],
+    COLOR: 13
 }
 
 Class.oneWayWallDown = {
     PARENT: "wall",
-    LABEL: "One-Way Wall (Down)"
+    LABEL: "One-Way Wall (Down)",
+    COLOR: 17
 }
 
 Class.oneWayWallLeft = {
     PARENT: "wall",
-    LABEL: "One-Way Wall (Left)"
+    LABEL: "One-Way Wall (Left)",
+    COLOR: 17
 }
 
 Class.oneWayWallRight = {
     PARENT: "wall",
-    LABEL: "One-Way Wall (Right)"
+    LABEL: "One-Way Wall (Right)",
+    COLOR: 17
+}
+
+Class.oneWayWallUp = {
+    PARENT: "wall",
+    LABEL: "One-Way Wall (Up)",
+    COLOR: 17
 }
 
 Class.stickyWall = {
     PARENT: "wall",
-    LABEL: "Sticky Wall"
+    LABEL: "Sticky Wall",
+    COLOR: 6
 }
 
 Class.trickWall = {
     PARENT: "wall",
-    LABEL: "Trick Wall"
+    LABEL: "Trick Wall",
+    COLOR: 4
 }
 
+Class.paintWallLayer = {
+    COLOR: 6,
+    SHAPE: 4
+}
 Class.paintWall = {
     PARENT: "wall",
     LABEL: "Paint Wall",
+    COLOR: 6,
     PROPS: [
-    ]
+        {
+            TYPE: 'paintWallLayer',
+            POSITION: {
+                SIZE: 20 * Math.SQRT1_2,
+                ANGLE: 45,
+                LAYER: 1
+            }
+        }
+    ],
 }
 
+Class.filterWallLayer = {
+    COLOR: 6,
+    SHAPE: 4
+}
 Class.filterWall = {
     PARENT: "wall",
-    LABEL: "Filter Wall"
+    LABEL: "Filter Wall",
+    COLOR: 6,
+    PROPS: [
+        {
+            TYPE: 'filterWallLayer',
+            POSITION: {
+                SIZE: 20 * Math.SQRT1_2,
+                ANGLE: 45,
+                LAYER: 1
+            }
+        }
+    ],
 }
 
 Class.teamWall = {
     PARENT: "wall",
-    LABEL: "Team Wall"
+    LABEL: "Team Wall",
+    COLOR: 12
 }
 
 Class.baseWall = {
     PARENT: "wall",
-    LABEL: "Base Wall"
+    LABEL: "Base Wall",
+    COLOR: 12
 }
 
 Class.portalWall = {
     PARENT: "wall",
-    LABEL: "Portal Wall"
+    LABEL: "Portal Wall",
+    COLOR: 10
 }
 
 Class.checkpointWall = {
     PARENT: "wall",
-    LABEL: "Checkpoint Wall"
+    LABEL: "Checkpoint Wall",
+    COLOR: 11
 }
