71e28a26c78f2a168f334b3dc01c5beece58e7a4
TGS <93544652+zephiii@users.noreply.github.com>
2026-02-17 01:30:07 +0000

    misc gone

--- server/lib/definitions/groups/tanks/special.js
+++ server/lib/definitions/groups/tanks/special.js
@@ -1,6 +1,58 @@
-const {combineStats, skillSet, makePolyhedron, weaponArray} = require('../facilitators.js')
-const {base, statnames, dfltskl, smshskl} = require('../constants.js')
-const g = require('../gunvals.js')
+const {combineStats, skillSet, weaponArray} = require('../../facilitators.js')
+const {base, statnames} = require('../../constants.js')
+const g = require('../../gunvals.js')
+
+// Base Protector
+Class.baseProtector = {
+    PARENT: "genericTank",
+    LABEL: "Base",
+    UPGRADE_LABEL: "Base Protector",
+    ON_MINIMAP: false,
+    SIZE: 64,
+    DAMAGE_CLASS: 0,
+    ACCEPTS_SCORE: false,
+    CAN_BE_ON_LEADERBOARD: false,
+    IGNORED_BY_AI: true,
+    HITS_OWN_TYPE: "pushOnlyTeam",
+    SKILL: skillSet({
+        rld: 1,
+        dam: 1,
+        pen: 1,
+        spd: 1,
+        str: 1,
+    }),
+    BODY: {
+        SPEED: 0,
+        HEALTH: 1e4,
+        DAMAGE: 10,
+        PENETRATION: 0.25,
+        SHIELD: 1e3,
+        REGEN: 100,
+        FOV: 1,
+        PUSHABILITY: 0,
+        RESIST: 10000,
+        HETERO: 0,
+    },
+    FACING_TYPE: ["spin", {speed: 0.04}],
+    TURRETS: [
+        {
+            POSITION: [25, 0, 0, 0, 360, 0],
+            TYPE: "dominationBody",
+        },
+        ...weaponArray({
+            POSITION: [12, 7, 0, 45, 100, 0],
+            TYPE: "baseSwarmTurret",
+        }, 4)
+    ],
+    GUNS: weaponArray([
+        {
+            POSITION: [4.5, 11.5, -1.3, 6, 0, 45, 0],
+        },
+        {
+            POSITION: [4.5, 8.5, -1.5, 7, 0, 45, 0],
+        },
+    ], 4)
+}
 
 // Dominators
 Class.dominator = {
@@ -200,57 +252,7 @@ for (let tier of sancTiers) {
     })
 }
 
-// Miscellaneous
-Class.baseProtector = {
-    PARENT: "genericTank",
-    LABEL: "Base",
-    UPGRADE_LABEL: "Base Protector",
-    ON_MINIMAP: false,
-    SIZE: 64,
-    DAMAGE_CLASS: 0,
-    ACCEPTS_SCORE: false,
-    CAN_BE_ON_LEADERBOARD: false,
-    IGNORED_BY_AI: true,
-    HITS_OWN_TYPE: "pushOnlyTeam",
-    SKILL: skillSet({
-        rld: 1,
-        dam: 1,
-        pen: 1,
-        spd: 1,
-        str: 1,
-    }),
-    BODY: {
-        SPEED: 0,
-        HEALTH: 1e4,
-        DAMAGE: 10,
-        PENETRATION: 0.25,
-        SHIELD: 1e3,
-        REGEN: 100,
-        FOV: 1,
-        PUSHABILITY: 0,
-        RESIST: 10000,
-        HETERO: 0,
-    },
-    FACING_TYPE: ["spin", {speed: 0.04}],
-    TURRETS: [
-        {
-            POSITION: [25, 0, 0, 0, 360, 0],
-            TYPE: "dominationBody",
-        },
-        ...weaponArray({
-            POSITION: [12, 7, 0, 45, 100, 0],
-            TYPE: "baseSwarmTurret",
-        }, 4)
-    ],
-    GUNS: weaponArray([
-        {
-            POSITION: [4.5, 11.5, -1.3, 6, 0, 45, 0],
-        },
-        {
-            POSITION: [4.5, 8.5, -1.5, 7, 0, 45, 0],
-        },
-    ], 4)
-}
+// Mothership
 Class.mothership = {
     PARENT: "genericTank",
     LABEL: "Mothership",
@@ -305,231 +307,20 @@ Class.mothership = {
     ], 8, 1/16)
 }
 Class.flagship = {
-    PARENT: "mothership",
+    PARENT: 'mothership',
     LABEL: "Flagship",
     NAME: "Flagship",
     TURRETS: [
         {
-            POSITION: [ 10, 0, 0, 45, 0, 1 ],
-            TYPE: "flagshipTurret"
-        }
-    ]
-}
-Class.arenaCloser = {
-    PARENT: "genericTank",
-    LABEL: "Arena Closer",
-    DISPLAY_NAME: true,
-    DANGER: 10,
-    SIZE: 34,
-    COLOR: "yellow",
-    UPGRADE_COLOR: "yellow",
-    LAYER: 13,
-    BODY: {
-        REGEN: 1e5,
-        HEALTH: 1e6,
-        DENSITY: 30,
-        DAMAGE: 1e5,
-        FOV: 10,
-        SPEED: 6,
-    },
-    SKILL: skillSet({rld: 1, dam: 1, pen: 1, str: 1, spd: 1, atk: 1, hlt: 1, shi: 1, rgn: 1, mob: 1}),
-    DRAW_HEALTH: false,
-    HITS_OWN_TYPE: "never",
-    ARENA_CLOSER: true,
-    IS_IMMUNE_TO_TILES: true,
-    UPGRADE_TOOLTIP: "Hackerman",
-    GUNS: [
-        {
+            TYPE: 'flagshipTurret',
             POSITION: {
-                LENGTH: 14,
-                WIDTH: 10
-            },
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.8, recoil: 0.25, health: 1e3, damage: 1e3, pen: 1e3, speed: 2.5, maxSpeed: 1.15, range: 1.8, density: 4, spray: 0.25}]),
-                TYPE: ["bullet", {LAYER: 12}]
+                SIZE: 10,
+                ANGLE: 45,
+                LAYER: 1
             }
         }
     ]
 }
-Class.antiTankMachineGun = {
-    PARENT: "dominator",
-    LABEL: "Anti-Tank Machine Gun",
-    UPGRADE_LABEL: "A.T.M.G.",
-    CONTROLLERS: [['spin', {onlyWhenIdle: true}], 'nearestDifferentMaster'],
-    LEVEL: 45,
-    SIZE: 32,
-    BODY: {
-        RESIST: 100,
-        SPEED: 1.32,
-        ACCELERATION: 0.8,
-        HEALTH: 1e99,
-        DAMAGE: 6,
-        PENETRATION: 0.25,
-        FOV: 1.35,
-        PUSHABILITY: 0,
-        HETERO: 0,
-        SHIELD: base.SHIELD * 1.4,
-    },
-    SKILL_CAP: Array(10).fill(15),
-    SKILL: Array(10).fill(15),
-    GUNS: [
-		{
-			POSITION: { LENGTH: 15, WIDTH: 3.0000001192092896, ASPECT: 0, X: -6.556708751634699e-8, Y: 1.5000000596046434, ANGLE: 0 },
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.op, {reload: 0.5, health: 100, damage: 100, recoil: 0, spray: 0.1, speed: 2, maxSpeed: 2}]),
-                TYPE: "bullet",
-            }
-		},
-		{
-			POSITION: { LENGTH: 15, WIDTH: 3.0000001192092896, ASPECT: 0, X: -6.556708770004402e-8, Y: -1.5000000596046434, ANGLE: 0 },
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.op, {reload: 0.5, health: 100, damage: 100, recoil: 0, spray: 0.1, speed: 2, maxSpeed: 2}]),
-                TYPE: "bullet",
-            }
-		},
-		{
-			POSITION: { LENGTH: 17.000000476837158, WIDTH: 3.0000001192092896, ASPECT: 0, X: 0, Y: 0, ANGLE: 0 },
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.op, {reload: 0.5, health: 100, damage: 100, recoil: 0, spray: 0.1, speed: 2, maxSpeed: 2}]),
-                TYPE: "bullet",
-            }
-		},
-		{
-			POSITION: { LENGTH: 10, WIDTH: 8.00000011920929, ASPECT: -1.2000000476837158, X: 9.999999999999998, Y: -6.123234262925839e-16, ANGLE: 90.00000250447816 }
-		},
-		{
-			POSITION: { LENGTH: 10, WIDTH: 8.00000011920929, ASPECT: -1.2000000476837158, X: 9.999999999999998, Y: -6.123233601181349e-16, ANGLE: -90.00000250447816 }
-		},
-		{
-			POSITION: { LENGTH: 5, WIDTH: 6.000000238418579, ASPECT: -1.600000023841858, X: 7.5, Y: -4.592425496802574e-16, ANGLE: 0 }
-		}
-    ],
-    TURRETS: [{
-        POSITION: [20, 0, 25, 0, 180, 1],
-        TYPE: ["antiTankMachineGunArm"]
-    }, {
-        POSITION: [20, 0, -25, 0, 180, 1],
-        TYPE: ["antiTankMachineGunArm"]
-    }, {
-        POSITION: [25, 0, 0, 0, 360, 0],
-        TYPE: ["dominationBody"]
-    }]
-}
-
-// CX-ATMG
-Class.cxATMGBullet = {PARENT: "bullet", SHAPE: Class.cube.SHAPE}
-Class.cxATMGArm = {
-    PARENT: "genericTank",
-    COLOR: "white",
-    SHAPE: Class.cube.SHAPE,
-    SKILL_CAP: Array(10).fill(15),
-    SKILL: Array(10).fill(15),
-    GUNS: [
-        {
-            POSITION: [15, 2.5, 1, 0, 2, 0, 0.2],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
-                TYPE: "cxATMGBullet",
-            }
-        },
-        {
-            POSITION: [15, 2.5, 1, 0, -2, 0, 0.2],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
-                TYPE: "cxATMGBullet",
-            }
-        },
-        {
-            POSITION: [1, 2.5, 1, 0, 0, 0, 0],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
-                TYPE: "cxATMGBullet",
-            }
-        },
-        {
-            POSITION: [16.5, 3.5, 1, 0, 0, 0, 0],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
-                TYPE: "cxATMGBullet",
-            }
-        },
-        {
-            POSITION: [5.5, 6.5, -1.8, 6.5, 0, 0, 0]
-        }
-    ],
-}
-Class.cxATMG = {
-    PARENT: "dominator",
-    LABEL: "CX-ATMG",
-    UPGRADE_LABEL: "CX-ATMG",
-    SHAPE: Class.cube.SHAPE,
-    SIZE: 12,
-    BODY: {
-        RESIST: 2,
-        SPEED: 2.32,
-        ACCELERATION: 0.8,
-        HEALTH: 200,
-        DAMAGE: 6,
-        PENETRATION: 0.25,
-        FOV: 1.35,
-        PUSHABILITY: 0,
-        HETERO: 0,
-        SHIELD: base.SHIELD * 1.4,
-    },
-    SKILL_CAP: Array(10).fill(15),
-    SKILL: Array(10).fill(15),
-    GUNS: [
-        {
-            POSITION: [15, 2.5, 1, 0, 2, 0, 0.2],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
-                TYPE: "cxATMGBullet",
-            }
-        },
-        {
-            POSITION: [15, 2.5, 1, 0, -2, 0, 0.2],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
-                TYPE: "cxATMGBullet",
-            }
-        },
-        {
-            POSITION: [1, 2.5, 1, 0, 0, 0, 0],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
-                TYPE: "cxATMGBullet",
-            }
-        },
-        {
-            POSITION: [16.5, 3.5, 1, 0, 0, 0, 0],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
-                TYPE: "cxATMGBullet",
-            }
-        },
-        {
-            POSITION: [24, 7, -1.3, 0, 0, 90, 0],
-        },
-        {
-            POSITION: [24, 7, -1.3, 0, 0, -90, 0],
-        },
-        {
-            POSITION: [5.5, 6.5, -1.8, 6.5, 0, 0, 0]
-        }
-    ],
-    TURRETS: [{
-        POSITION: [20, 0, 25, 0, 180, 1],
-        TYPE: ["cxATMGArm"]
-    }, {
-        POSITION: [20, 0, -25, 0, 180, 1],
-        TYPE: ["cxATMGArm"]
-    }, {
-        POSITION: [26, 0, 0, 0, 360, 0],
-        TYPE: ["dominationBody"]
-    }]
-}
-
-// Turkey
 Class.turkeynose = {
     COLOR: 19,
     LABEL: '',
@@ -671,50 +462,218 @@ Class.turkey = {
         }
     ],
 }
-Class.fat456 = {
-    PARENT: "genericTank",
-    SIZE: 30,
-    LABEL: "Fat456",
-    COLOR: "brown", // should be pureblack but just the outline
-    FACING_TYPE: "spin",
+
+// ATMG
+Class.antiTankMachineGun = {
+    PARENT: "dominator",
+    LABEL: "Anti-Tank Machine Gun",
+    UPGRADE_LABEL: "A.T.M.G.",
+    CONTROLLERS: [['spin', {onlyWhenIdle: true}], 'nearestDifferentMaster'],
+    LEVEL: 45,
+    SIZE: 32,
     BODY: {
-        SPEED: base.SPEED * 4
+        RESIST: 100,
+        SPEED: 1.32,
+        ACCELERATION: 0.8,
+        HEALTH: 1e99,
+        DAMAGE: 6,
+        PENETRATION: 0.25,
+        FOV: 1.35,
+        PUSHABILITY: 0,
+        HETERO: 0,
+        SHIELD: base.SHIELD * 1.4,
     },
-    TURRETS: [
+    SKILL_CAP: Array(10).fill(15),
+    SKILL: Array(10).fill(15),
+    GUNS: [
         {
-            POSITION: [12, 8, 0, 0, 190, 0],
-            TYPE: "architectGun",
+            POSITION: { LENGTH: 15, WIDTH: 3.0000001192092896, ASPECT: 0, X: -6.556708751634699e-8, Y: 1.5000000596046434, ANGLE: 0 },
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.op, {reload: 0.5, health: 100, damage: 100, recoil: 0, spray: 0.1, speed: 2, maxSpeed: 2}]),
+                TYPE: "bullet",
+            }
         },
         {
-            POSITION: [12, 8, 0, 120, 190, 0],
-            TYPE: "architectGun",
+            POSITION: { LENGTH: 15, WIDTH: 3.0000001192092896, ASPECT: 0, X: -6.556708770004402e-8, Y: -1.5000000596046434, ANGLE: 0 },
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.op, {reload: 0.5, health: 100, damage: 100, recoil: 0, spray: 0.1, speed: 2, maxSpeed: 2}]),
+                TYPE: "bullet",
+            }
         },
         {
-            POSITION: [12, 8, 0, 240, 190, 0],
-            TYPE: "architectGun",
+            POSITION: { LENGTH: 17.000000476837158, WIDTH: 3.0000001192092896, ASPECT: 0, X: 0, Y: 0, ANGLE: 0 },
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.op, {reload: 0.5, health: 100, damage: 100, recoil: 0, spray: 0.1, speed: 2, maxSpeed: 2}]),
+                TYPE: "bullet",
+            }
         },
+        {
+            POSITION: { LENGTH: 10, WIDTH: 8.00000011920929, ASPECT: -1.2000000476837158, X: 9.999999999999998, Y: -6.123234262925839e-16, ANGLE: 90.00000250447816 }
+        },
+        {
+            POSITION: { LENGTH: 10, WIDTH: 8.00000011920929, ASPECT: -1.2000000476837158, X: 9.999999999999998, Y: -6.123233601181349e-16, ANGLE: -90.00000250447816 }
+        },
+        {
+            POSITION: { LENGTH: 5, WIDTH: 6.000000238418579, ASPECT: -1.600000023841858, X: 7.5, Y: -4.592425496802574e-16, ANGLE: 0 }
+        }
+    ],
+    TURRETS: [{
+        POSITION: [20, 0, 25, 0, 180, 1],
+        TYPE: ["antiTankMachineGunArm"]
+    }, {
+        POSITION: [20, 0, -25, 0, 180, 1],
+        TYPE: ["antiTankMachineGunArm"]
+    }, {
+        POSITION: [25, 0, 0, 0, 360, 0],
+        TYPE: ["dominationBody"]
+    }]
+}
+Class.cxATMGBullet = {PARENT: "bullet", SHAPE: Class.cube.SHAPE}
+Class.cxATMGArm = {
+    PARENT: "genericTank",
+    COLOR: "white",
+    SHAPE: Class.cube.SHAPE,
+    SKILL_CAP: Array(10).fill(15),
+    SKILL: Array(10).fill(15),
+    GUNS: [
+        {
+            POSITION: [15, 2.5, 1, 0, 2, 0, 0.2],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
+                TYPE: "cxATMGBullet",
+            }
+        },
+        {
+            POSITION: [15, 2.5, 1, 0, -2, 0, 0.2],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
+                TYPE: "cxATMGBullet",
+            }
+        },
+        {
+            POSITION: [1, 2.5, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
+                TYPE: "cxATMGBullet",
+            }
+        },
+        {
+            POSITION: [16.5, 3.5, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
+                TYPE: "cxATMGBullet",
+            }
+        },
+        {
+            POSITION: [5.5, 6.5, -1.8, 6.5, 0, 0, 0]
+        }
     ],
 }
-Class.wifeBeater = {
-    PARENT: "overlord",
-    LABEL: 'Wife Beater',
-    DANGER: 8,
-    STAT_NAMES: statnames.drone,
+Class.cxATMG = {
+    PARENT: "dominator",
+    LABEL: "CX-ATMG",
+    UPGRADE_LABEL: "CX-ATMG",
+    SHAPE: Class.cube.SHAPE,
+    SIZE: 12,
     BODY: {
-        ACCELERATION: base.ACCEL * 0.75,
-        SPEED: base.SPEED * 0.8,
-        FOV: base.FOV * 1.1,
+        RESIST: 2,
+        SPEED: 2.32,
+        ACCELERATION: 0.8,
+        HEALTH: 200,
+        DAMAGE: 6,
+        PENETRATION: 0.25,
+        FOV: 1.35,
+        PUSHABILITY: 0,
+        HETERO: 0,
+        SHIELD: base.SHIELD * 1.4,
     },
-    MAX_CHILDREN: 16,
-    GUNS: weaponArray({
-        POSITION: [6, 12, 1.2, 8, 0, 0, 0],
-        PROPERTIES: {
-            SHOOT_SETTINGS: combineStats([g.drone, g.overseer, g.op]),
-            TYPE: "drone",
-            AUTOFIRE: true,
-            SYNCS_SKILLS: true,
-            STAT_CALCULATOR: "drone",
-            WAIT_TO_CYCLE: true
+    SKILL_CAP: Array(10).fill(15),
+    SKILL: Array(10).fill(15),
+    GUNS: [
+        {
+            POSITION: [15, 2.5, 1, 0, 2, 0, 0.2],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
+                TYPE: "cxATMGBullet",
+            }
+        },
+        {
+            POSITION: [15, 2.5, 1, 0, -2, 0, 0.2],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
+                TYPE: "cxATMGBullet",
+            }
+        },
+        {
+            POSITION: [1, 2.5, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
+                TYPE: "cxATMGBullet",
+            }
+        },
+        {
+            POSITION: [16.5, 3.5, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.5}]),
+                TYPE: "cxATMGBullet",
+            }
+        },
+        {
+            POSITION: [24, 7, -1.3, 0, 0, 90, 0],
+        },
+        {
+            POSITION: [24, 7, -1.3, 0, 0, -90, 0],
+        },
+        {
+            POSITION: [5.5, 6.5, -1.8, 6.5, 0, 0, 0]
         }
-    }, 4)
+    ],
+    TURRETS: [{
+        POSITION: [20, 0, 25, 0, 180, 1],
+        TYPE: ["cxATMGArm"]
+    }, {
+        POSITION: [20, 0, -25, 0, 180, 1],
+        TYPE: ["cxATMGArm"]
+    }, {
+        POSITION: [26, 0, 0, 0, 360, 0],
+        TYPE: ["dominationBody"]
+    }]
+}
+
+// Arena Closer
+Class.arenaCloser = {
+    PARENT: "genericTank",
+    LABEL: "Arena Closer",
+    DISPLAY_NAME: true,
+    DANGER: 10,
+    SIZE: 34,
+    COLOR: "yellow",
+    UPGRADE_COLOR: "yellow",
+    LAYER: 13,
+    BODY: {
+        REGEN: 1e5,
+        HEALTH: 1e6,
+        DENSITY: 30,
+        DAMAGE: 1e5,
+        FOV: 10,
+        SPEED: 6,
+    },
+    SKILL: skillSet({rld: 1, dam: 1, pen: 1, str: 1, spd: 1, atk: 1, hlt: 1, shi: 1, rgn: 1, mob: 1}),
+    DRAW_HEALTH: false,
+    HITS_OWN_TYPE: "never",
+    ARENA_CLOSER: true,
+    IS_IMMUNE_TO_TILES: true,
+    UPGRADE_TOOLTIP: "Hackerman",
+    GUNS: [
+        {
+            POSITION: {
+                LENGTH: 14,
+                WIDTH: 10
+            },
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, {reload: 0.8, recoil: 0.25, health: 1e3, damage: 1e3, pen: 1e3, speed: 2.5, maxSpeed: 1.15, range: 1.8, density: 4, spray: 0.25}]),
+                TYPE: ["bullet", {LAYER: 12}]
+            }
+        }
+    ]
 }
