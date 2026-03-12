43e0d2ce5933e253b2378d7207778efb27cb1a97
TGS <93544652+zephiii@users.noreply.github.com>
2026-03-12 20:13:20 +0000

    temporarily remove ar/diep content
    release won't have this

--- server/Game/gamemodes/scripts/mothership.js
+++ server/Game/gamemodes/scripts/mothership.js
@@ -2,8 +2,6 @@ class Mothership {
     constructor() {
         if (Config.thanksgiving) {
             this.choices = ["turkey"];
-        } else if (Config.arms_race) {
-            this.choices = ["flagship"];
         } else {
             this.choices = ["mothership"];
         }
@@ -58,8 +56,6 @@ class Mothership {
             o.team = team;
             if (Config.thanksgiving) {
                 o.name = "Turkey";
-            } else if (Config.arms_race) {
-                o.name = "Flagship";
             } else {
                 o.name = "Mothership";
             }
