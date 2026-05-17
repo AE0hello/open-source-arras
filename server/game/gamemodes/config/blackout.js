264f467cc7c89a950bf37cdb3302a97dd4b709ee
TGS <93544652+zephiii@users.noreply.github.com>
2026-05-17 07:49:34 +0000

    blackout fix

--- server/game/gamemodes/config/blackout.js
+++ server/game/gamemodes/config/blackout.js
@@ -1,8 +1,5 @@
-let blackout_fog = "#000000";
-let minimap_glitching_color = "pureBlack";
-
 module.exports = {
     blackout: true,
-    blackout_fog_color: blackout_fog,
-    blackout_minimap_color: minimap_glitching_color,
+    blackout_fog: '#000000',
+    blackout_minimap_color: '#484848'
 }
