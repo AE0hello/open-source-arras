e7fc8f4f0819719108ab4a1a48b8db60844f0a35
TGS <93544652+zephiii@users.noreply.github.com>
2026-06-10 21:37:24 +0000

    Reapply "Fix necro"
    This reverts commit 570214b6f474bd201396b9d6ddb89c91ca701729.

--- server/game/entities/bulletEntity.js
+++ server/game/entities/bulletEntity.js
@@ -196,35 +196,6 @@ class bulletEntity { // Basically an (Entity) but with heavy limitations to impr
         if (set.CLEAR_ON_MASTER_UPGRADE != null) this.settings.clearOnMasterUpgrade = set.CLEAR_ON_MASTER_UPGRADE;
         if (set.HEALTH_WITH_LEVEL != null) this.settings.healthWithLevel = set.HEALTH_WITH_LEVEL;
         if (set.OBSTACLE != null) this.settings.obstacle = set.OBSTACLE;
-        if (set.NECRO != null) {
-            this.settings.necroTypes = Array.isArray(set.NECRO) ? set.NECRO : set.NECRO ? [this.shape] : [];
-
-            // Necro function for tanks
-            this.settings.necroDefineGuns = {};
-            for (let shape of this.settings.necroTypes) {
-                // Pick the first gun with the right necroType to use for stats and use its defineBullet function
-                this.settings.necroDefineGuns[shape] = Array.from(this.guns).filter((gun) => gun[1].bulletType.NECRO && (gun[1].bulletType.NECRO === shape || (gun[1].bulletType.NECRO === true && gun[1].bulletType.SHAPE === this.shape) || gun[1].bulletType.NECRO.includes(shape)))[0];
-            }
-
-            this.necro = (host) => {
-                let gun = this.settings.necroDefineGuns[host.shape];
-                if (!gun || !gun.checkShootPermission()) return false;
-
-                let savedFacing = host.facing;
-                let savedSize = host.SIZE;
-                
-                host.controllers = [];
-                host.define("genericEntity");
-                gun.bulletInit(host);
-                host.team = this.master.master.team;
-                host.master = this.master;
-                host.color.base = this.color.base;
-                host.facing = savedFacing;
-                host.SIZE = savedSize;
-                host.health.amount = host.health.max;
-                return true;
-            }
-        }
         if (set.HAS_NO_RECOIL != null) this.settings.hasNoRecoil = set.HAS_NO_RECOIL;
         if (set.CRAVES_ATTENTION != null) this.settings.attentionCraver = set.CRAVES_ATTENTION;
         if (set.HEALER) this.healer = true;
@@ -280,6 +251,35 @@ class bulletEntity { // Basically an (Entity) but with heavy limitations to impr
             }
             for (let guns of newGuns) {
                 this.guns.set(guns.id, guns);
+            }
+        }
+        if (set.NECRO != null) {
+            this.settings.necroTypes = Array.isArray(set.NECRO) ? set.NECRO : set.NECRO ? [this.shape] : [];
+
+            // Necro function for tanks
+            this.settings.necroDefineGuns = {};
+            for (let shape of this.settings.necroTypes) {
+                // Pick the first gun with the right necroType to use for stats and use its defineBullet function
+                this.settings.necroDefineGuns[shape] = Array.from(this.guns.values()).filter((gun) => gun.bulletType.NECRO && (gun.bulletType.NECRO === shape || (gun.bulletType.NECRO === true && gun.bulletType.SHAPE === this.shape) || gun.bulletType.NECRO.includes(shape)))[0];
+            }
+            
+            this.necro = (host) => {
+                let gun = this.settings.necroDefineGuns[host.shape];
+                if (!gun || !gun.checkShootPermission()) return false;
+
+                let savedFacing = host.facing;
+                let savedSize = host.SIZE;
+                
+                host.controllers = [];
+                host.define("genericEntity");
+                gun.bulletInit(host);
+                host.team = this.master.master.team;
+                host.master = this.master;
+                host.color.base = this.color.base;
+                host.facing = savedFacing;
+                host.SIZE = savedSize;
+                host.health.amount = host.health.max;
+                return true;
             }
         }
         if (set.EXTRA_SKILL != null) this.skill.points += set.EXTRA_SKILL;
