38513b31e89baf3675bfa1aba184b124232d4af8
AE0hello <141400718+AE0hello@users.noreply.github.com>
2026-06-10 23:14:14 +0200

    S P A C E

--- server/game/entities/entity.js
+++ server/game/entities/entity.js
@@ -533,6 +533,7 @@ class Entity extends EventEmitter {
                 // Pick the first gun with the right necroType to use for stats and use its defineBullet function
                 this.settings.necroDefineGuns[shape] = this.gunsArrayed.filter((gun) => gun.bulletType.NECRO && (gun.bulletType.NECRO === shape || (gun.bulletType.NECRO === true && gun.bulletType.SHAPE === this.shape) || gun.bulletType.NECRO.includes(shape)))[0];
             }
+            
             this.necro = (host) => {
                 let gun = this.settings.necroDefineGuns[host.shape];
                 if (!gun || !gun.checkShootPermission()) return false;
