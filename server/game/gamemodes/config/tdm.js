8c8721285103dcd1a3ac92dfdf3f23596ef7565a
TGS <93544652+TheGreenSquare@users.noreply.github.com>
2026-06-13 07:00:39 +0000

    delete this for now

--- server/game/gamemodes/config/tdm.js
+++ server/game/gamemodes/config/tdm.js
@@ -1,14 +1,6 @@
-const team_count = Config.teams ?? Math.floor(Math.random() * 2 + 1) * 2
-
-maze = team_count % 2 !== 0 ? team_count + 1 : team_count
-if (maze == 6) {
-    maze = 8
-}
-
 module.exports = {
     mode: "tdm",
-    teams: team_count,
-    maze_type: maze,
+    teams: Config.teams ?? Math.floor(Math.random() * 2 + 1) * 2,
     do_not_override_room: true,
     room_setup: ["room_tdm"]
 }
