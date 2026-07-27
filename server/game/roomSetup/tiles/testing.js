e2a33001c403f6c3d089c1c821b3a307f57144be
TGS <93544652+TheGreenSquare@users.noreply.github.com>
2026-07-27 16:15:13 +0000

    remove unused img files

--- server/game/roomSetup/tiles/testing.js
+++ server/game/roomSetup/tiles/testing.js
@@ -1,21 +1,11 @@
 // IMAGE TESTING
-tileClass.imagetest2 = new Tile({
-    COLOR: "white",
-    IMAGE: "/tileGrass1.jpg",
-    NAME: "Image Tile",
-});
 tileClass.imagetest3 = new Tile({
     COLOR: "white",
-    IMAGE: "/Grass_c_0.png",
-    NAME: "Image Tile",
-});
-tileClass.imagetest4 = new Tile({
-    COLOR: "white",
-    IMAGE: "/Grass_c_0.svg",
+    IMAGE: "/round.png",
     NAME: "Image Tile",
 });
 tileClass.imagetesterror = new Tile({
     COLOR: "white",
-    IMAGE: "/noneofyourbusiness.png", // TODO: Hardcode the new missing tile texture as an svg instead of just pointing to a file
+    IMAGE: "/noneofyourbusiness.png", // TODO: Hardcode the missing tile texture as an svg instead of just pointing to a file
     NAME: "Missing Tile",
 });
