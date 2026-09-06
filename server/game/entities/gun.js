58e3253b788e81acd6abfd08d38995104b4c014d
TGS <93544652+TheGreenSquare@users.noreply.github.com>
2026-09-06 16:22:47 +0000

    less janky destroy oldest fix + optimizing

--- server/game/entities/gun.js
+++ server/game/entities/gun.js
@@ -252,23 +252,23 @@ class Gun extends EventEmitter {
         }
     }
     checkShootPermission() {
-        let sk = this.bulletStats === "master" ? this.body.skill : this.bulletStats;
-        let shootPermission = this.countsOwnKids
-            ? this.countsOwnKids >
-                this.children.length * (this.calculator == "necro" ? sk.rld : 1)
-            : this.body.maxChildren
-            ? this.body.maxChildren >
-                this.body.children.length * (this.calculator == "necro" ? sk.rld : 1)
-            : true;
+        let skill = this.bulletStats === 'master' ? this.body.skill : this.bulletStats;
+        let necroReload = this.calculator === 'necro' ? skill.rld : 1;
+        let shootPermission = true;
 
-        // Handle destroying oldest child
+        if (this.countsOwnKids) {
+            shootPermission = this.countsOwnKids + this.destroyOldestChild > this.children.length * necroReload;
+        } else if (this.body.maxChildren) {
+            shootPermission = this.body.maxChildren > this.body.children.length * necroReload;
+        };
+
         if (this.destroyOldestChild && !shootPermission) {
             shootPermission = true;
             this.destroyOldest();
-        }
+        };
 
         return shootPermission;
-    }
+    };
     destroyOldest() {
         let oldestChild,
             oldestTime = Infinity;
