13264f0be0db028d14930405cd836e7a177cb2b2
TGS <93544652+TheGreenSquare@users.noreply.github.com>
2026-09-06 16:13:03 +0000

    incredibly janky re-fix

--- server/game/entities/gun.js
+++ server/game/entities/gun.js
@@ -252,23 +252,26 @@ class Gun extends EventEmitter {
         }
     }
     checkShootPermission() {
-        let sk = this.bulletStats === "master" ? this.body.skill : this.bulletStats;
-        let shootPermission = this.countsOwnKids
-            ? this.countsOwnKids >
-                this.children.length * (this.calculator == "necro" ? sk.rld : 1)
-            : this.body.maxChildren
-            ? this.body.maxChildren >
-                this.body.children.length * (this.calculator == "necro" ? sk.rld : 1)
-            : true;
+        let skill = this.bulletStats === 'master' ? this.body.skill : this.bulletStats;
+        let necroReload = this.calculator === 'necro' ? skill.rld : 1;
+        let shootPermission = true;
+        let doNotDestroy = true;
 
-        // Handle destroying oldest child
-        if (this.destroyOldestChild && !shootPermission) {
-            shootPermission = true;
+        if (this.countsOwnKids) {
+            shootPermission = this.countsOwnKids > this.children.length * necroReload;
+            doNotDestroy = this.countsOwnKids >= this.children.length * necroReload;
+        } else if (this.body.maxChildren) {
+            shootPermission = this.body.maxChildren > this.body.children.length * necroReload;
+            doNotDestroy = this.body.maxChildren >= this.body.children.length * necroReload;
+        };
+
+        if (this.destroyOldestChild && !doNotDestroy) {
+            doNotDestroy = true;
             this.destroyOldest();
-        }
+        };
 
         return shootPermission;
-    }
+    };
     destroyOldest() {
         let oldestChild,
             oldestTime = Infinity;
