11a98a409d66b6efd5be6ba4f46c4db0d15fe6d3
User75670 <u007549@gmail.com>
2026-09-08 18:14:53 +0200

    add globalStore in Entity

--- server/game/entities/entity.js
+++ server/game/entities/entity.js
@@ -104,6 +104,7 @@ class Entity extends EventEmitter {
         this.maxX = 0;
         this.maxY = 0;
         this.collidingBond = false;
+        this.globalStore = {};
         // Optimized AABB calculation and update
         this.updateAABB = (active) => {
             this.antiNaN.update();
